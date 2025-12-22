89e02882b1c8a778bc72ee4d4ac1dfe595ec1a8b
Akshay Govil <akshaygovil@Akshays-MacBook-Pro.local>
2025-12-22 11:53:06 +1100

    newest deployment

---
+++ src/app/components/privacy_policy/page.tsx
@@ -0,0 +1,9 @@
+export default function PrivacyPolicyPage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <p>
+        we changed the privacy policy page to here(i changed the privacy policy page
+      </p>
+    </main>
+  );
+}
